91207e8d8d775d069cc4a8ba8ce70a2c76d49e8d
MountZionProject <152500134+MountZionProject@users.noreply.github.com>
2023-11-30 18:00:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,178 @@
+
+      function kulathur()
+    {
+    var title=document.getElementById('title');
+        title.innerHTML="Kulathur Map";
+         var soil=document.getElementById('soil');
+          soil.innerHTML="Sandy Clay Loam Soil";
+        var water=document.getElementById('water');
+          water.innerHTML="Well";
+        var ponds=document.getElementById('ponds');
+          ponds.innerHTML="30(Approx.)";
+        var major=document.getElementById('major');
+          major.innerHTML="Paddy,Rose,Marigold, Tuberose, Plantain Trees";
+        var village=document.getElementById('village');
+        village.innerHTML="There are 73 Revenue Villages in Kulathur Taluk";
+       
+    }
+     function pdkt()
+    {
+   var title=document.getElementById('title');
+        title.innerHTML="Pudukkottai Map";
+         var soil=document.getElementById('soil');
+          soil.innerHTML="Sandy Clay Loam Soil";
+        var water=document.getElementById('water');
+          water.innerHTML="Well, river and borewell";
+        var ponds=document.getElementById('ponds');
+          ponds.innerHTML="30(Approx.)";
+        var major=document.getElementById('major');
+          major.innerHTML="Paddy,Rose,Marigold, Tuberose, Plantain Trees";
+        var village=document.getElementById('village');
+        village.innerHTML="There are 40 Revenue Villages in Pudukkottai Taluk";
+       
+    }
+    function iluppur()
+    {
+   var title=document.getElementById('title');
+        title.innerHTML="Illupur Map";
+         var soil=document.getElementById('soil');
+          soil.innerHTML="Sandy Clay Loam Soil";
+        var water=document.getElementById('water');
+          water.innerHTML="Well and borewell";
+        var ponds=document.getElementById('ponds');
+          ponds.innerHTML="20(Approx.)";
+        var major=document.getElementById('major');
+          major.innerHTML="Paddy,Bamboo,Rose, Plantain Trees";
+        var village=document.getElementById('village');
+        village.innerHTML="There are 58 Revenue Villages in Illuppur Taluk";
+       
+    }
+     function pon()
+    {
+   var title=document.getElementById('title');
+        title.innerHTML="Ponnamaravathy Map";
+         var soil=document.getElementById('soil');
+          soil.innerHTML="Sandy Clay Loam Soil";
+        var water=document.getElementById('water');
+          water.innerHTML="Well and borewell, rainfall";
+        var ponds=document.getElementById('ponds');
+          ponds.innerHTML="30(Approx.)";
+        var major=document.getElementById('major');
+          major.innerHTML="Paddy,Sesame,Rose,Marigold, Sugarcane,Tuberose, Plantain Trees";
+        var village=document.getElementById('village');
+        village.innerHTML="There are 49 Revenue Villages in Ponnamaravathy Taluk";
+       
+    }
+     function thirumayam()
+    {
+    var title=document.getElementById('title');
+        title.innerHTML="Thirumayam Map";
+         var soil=document.getElementById('soil');
+          soil.innerHTML="Sandy Clay Loam Soil";
+        var water=document.getElementById('water');
+          water.innerHTML="Well and Rainfall";
+        var ponds=document.getElementById('ponds');
+          ponds.innerHTML="15(Approx.)";
+        var major=document.getElementById('major');
+          major.innerHTML="Paddy,Coconut trees, Plantain Trees";
+        var village=document.getElementById('village');
+        village.innerHTML="There are 82 Revenue Villages in Thirumayam Taluk";
+       
+    }
+     function atg()
+    {
+   var title=document.getElementById('title');
+        title.innerHTML="Aranthangi Map";
+         var soil=document.getElementById('soil');
+          soil.innerHTML="Sandy Clay Loam Soil, Aluvial Soil";
+        var water=document.getElementById('water');
+          water.innerHTML="Well and borewell";
+        var ponds=document.getElementById('ponds');
+          ponds.innerHTML="35(Approx.)";
+        var major=document.getElementById('major');
+          major.innerHTML="Paddy,Sugarcane,Coconut trees, Plantain Trees";
+        var village=document.getElementById('village');
+        village.innerHTML="There are 105 Revenue Villages in Aranthangi Taluk";
+       
+    }
+     function alangudi()
+    {
+    var title=document.getElementById('title');
+        title.innerHTML="Alangudi Map";
+         var soil=document.getElementById('soil');
+          soil.innerHTML="Sandy Clay Loam Soil";
+        var water=document.getElementById('water');
+          water.innerHTML="Well and Rainfall";
+        var ponds=document.getElementById('ponds');
+          ponds.innerHTML="30(Approx.)";
+        var major=document.getElementById('major');
+          major.innerHTML="Paddy,Rose,Marigold, Tuberose, Plantain Trees";
+        var village=document.getElementById('village');
+        village.innerHTML="There are 73 Revenue Villages in Alangudi Taluk";
+       
+    }
+     function gand()
+    {
+    var title=document.getElementById('title');
+        title.innerHTML="Gandharvakkottai Map";
+         var soil=document.getElementById('soil');
+          soil.innerHTML="Sandy Clay Loam Soil";
+        var water=document.getElementById('water');
+          water.innerHTML="Well,Borewell and rainfall";
+        var ponds=document.getElementById('ponds');
+          ponds.innerHTML="30(Approx.)";
+        var major=document.getElementById('major');
+          major.innerHTML="Paddy,Sesame,Rose,Marigold, Tuberose, Plantain Trees";
+        var village=document.getElementById('village');
+        village.innerHTML="There are 37 Revenue Villages in Gandharvakkottai Taluk";
+       
+    }
+     function karambakudi()
+    {
+   var title=document.getElementById('title');
+        title.innerHTML="Karambakudi Map";
+         var soil=document.getElementById('soil');
+          soil.innerHTML="Sandy Clay Loam Soil";
+        var water=document.getElementById('water');
+          water.innerHTML="Well";
+        var ponds=document.getElementById('ponds');
+          ponds.innerHTML="15(Approx.)";
+        var major=document.getElementById('major');
+          major.innerHTML="Paddy,Rose,Marigold, Tuberose, Plantain Trees";
+        var village=document.getElementById('village');
+        village.innerHTML="There are 50 Revenue Villages in Karambakudi Taluk";
+       
+    }
+     function avk()
+    {
+   var title=document.getElementById('title');
+        title.innerHTML="Avudaiyarkovil Map";
+         var soil=document.getElementById('soil');
+          soil.innerHTML="Sandy Clay Loam Soil";
+        var water=document.getElementById('water');
+          water.innerHTML="Well, borewell and rainfall";
+        var ponds=document.getElementById('ponds');
+          ponds.innerHTML="40(Approx.)";
+        var major=document.getElementById('major');
+          major.innerHTML="Paddy,Rose,Sugarcane, Marigold, Tuberose, Plantain Trees";
+        var village=document.getElementById('village');
+        village.innerHTML="There are 96 Revenue Villages in Avudaiyarkovil Taluk";
+       
+    }
+     function manamelkudi()
+    {
+   var title=document.getElementById('title');
+        title.innerHTML="Manamelkudi Map";
+         var soil=document.getElementById('soil');
+          soil.innerHTML="Sandy Clay Loam Soil";
+        var water=document.getElementById('water');
+          water.innerHTML="Well";
+        var ponds=document.getElementById('ponds');
+          ponds.innerHTML="30(Approx.)";
+        var major=document.getElementById('major');
+          major.innerHTML="Paddy,Rose,Marigold, Tuberose, Plantain Trees";
+        var village=document.getElementById('village');
+        village.innerHTML="There are 72 Revenue Villages in Manamelkudi Taluk";
+       
+    }
+   
